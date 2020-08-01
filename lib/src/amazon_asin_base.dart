1cce1289eda4e7661a17ee3d820903af5d4e291d
Don Nguyen <don@spiderbox.design>
2020-08-01 16:06:03 +0700

    Parse ASIN and url in string

--- lib/src/amazon_asin_base.dart
+++ lib/src/amazon_asin_base.dart
@@ -1,6 +1,8 @@
 class AmazonASIN {
   static const String REGEX_MATCH =
       "(B[0-9]{2}[0-9A-Z]{7}|[0-9]{9}(?:X|[0-9]))";
+  static const String URL_REGEX_MATCH =
+      r"((https?:www\.)|(https?:\/\/)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?";
 
   static Map<String, dynamic> parse(String urlOrASIN) {
     if (urlOrASIN.length == 10 || urlOrASIN.length == 13) {
@@ -8,11 +10,19 @@ class AmazonASIN {
     }
 
     var result;
+    var url;
     RegExp exp = RegExp(REGEX_MATCH);
+    RegExp urlExp = RegExp(URL_REGEX_MATCH);
 
-    if (exp.hasMatch(urlOrASIN)) {
-      var asin = exp.stringMatch(urlOrASIN);
-      result = _result(asin, urlOrASIN);
+    if (urlExp.hasMatch(urlOrASIN)) {
+      url = urlExp.stringMatch(urlOrASIN);
+    } else {
+      url = urlOrASIN;
+    }
+
+    if (exp.hasMatch(url)) {
+      var asin = exp.stringMatch(url);
+      result = _result(asin, url);
     } else {
       result = _result(null, null);
     }
@@ -27,10 +37,6 @@ class AmazonASIN {
       domain = Uri.parse(url)?.host;
     }
 
-    return {
-      "ASIN": ASIN,
-      "url": url,
-      "domain": domain
-    };
+    return {"ASIN": ASIN, "url": url, "domain": domain};
   }
 }
--- test/amazon_asin_test.dart
+++ test/amazon_asin_test.dart
@@ -2,22 +2,38 @@ import 'package:amazon_asin/amazon_asin.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ASIN, rul, tld', () {
-    var asin = AmazonASIN.parse("https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop");
+  test('ASIN, url, tld inside text block', () {
+    var asin = AmazonASIN.parse(
+        "Samsung Galaxy S9 G960U 64GB Unlocked GSM 4G LTE Phone w/ 12MP Camera - Midnight Black https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop");
 
     expect(asin, {
       "ASIN": "B01IG0E1F0",
-      "url": "https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop",
+      "url":
+          "https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop",
       "domain": "www.amazon.com"
     });
   });
 
-  test('ASIN, rul, tld for different Tld', () {
-    var asin = AmazonASIN.parse("https://www.amazon.co.uk/d/Hair-Gel/Eco-Styler-Olive-Oil-Styling/B003E7UNE4");
+  test('ASIN, url, tld', () {
+    var asin = AmazonASIN.parse(
+        "https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop");
+
+    expect(asin, {
+      "ASIN": "B01IG0E1F0",
+      "url":
+          "https://www.amazon.com/gp/product/B01IG0E1F0/ref=s9u_nwrsa_gw_i3?ie=UTF8&fpl=fresh&pd_rd_i=B01IG0E1F0&pd_rd_r=HX877ZSJ45VZ39AN59A1&pd_rd_w=6aVbp&pd_rd_wg=Yyiuk&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=K1JQVYXFAG8F4WKVT3N0&pf_rd_t=36701&pf_rd_p=b15e70a0-40cd-4167-b336-ff44ea910cf2&pf_rd_i=desktop",
+      "domain": "www.amazon.com"
+    });
+  });
+
+  test('ASIN, url, tld for different Tld', () {
+    var asin = AmazonASIN.parse(
+        "https://www.amazon.co.uk/d/Hair-Gel/Eco-Styler-Olive-Oil-Styling/B003E7UNE4");
 
     expect(asin, {
       "ASIN": "B003E7UNE4",
-      "url": "https://www.amazon.co.uk/d/Hair-Gel/Eco-Styler-Olive-Oil-Styling/B003E7UNE4",
+      "url":
+          "https://www.amazon.co.uk/d/Hair-Gel/Eco-Styler-Olive-Oil-Styling/B003E7UNE4",
       "domain": "www.amazon.co.uk"
     });
   });
@@ -25,20 +41,12 @@ void main() {
   test('ASIN only', () {
     var asin = AmazonASIN.parse("B01IG0E1F0");
 
-    expect(asin, {
-      "ASIN": "B01IG0E1F0",
-      "url": null,
-      "domain": null
-    });
+    expect(asin, {"ASIN": "B01IG0E1F0", "url": null, "domain": null});
   });
 
   test('Not valid ASIN found', () {
     var asin = AmazonASIN.parse("https://www.google.com");
 
-    expect(asin, {
-      "ASIN": null,
-      "url": null,
-      "domain": null
-    });
+    expect(asin, {"ASIN": null, "url": null, "domain": null});
   });
 }
